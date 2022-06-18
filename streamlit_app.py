915d68e00895afd51ef47711f4bf6e09539c605e
Mateusz Wiatr <55045247+MateuszWiatr@users.noreply.github.com>
2022-06-18 15:08:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My Parents New Healthy Diner')
 
@@ -15,3 +16,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
    
 
+steamlit.header('Fruityvice Fruit Advice!')
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+steamlit.text(fruityvicce_response.json())
