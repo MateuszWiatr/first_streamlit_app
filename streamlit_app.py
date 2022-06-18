d9c0694821ee2e0944f3aefb0fb8d267e41b22a2
Mateusz Wiatr <55045247+MateuszWiatr@users.noreply.github.com>
2022-06-18 16:04:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ fruits_selected = streamlit.multiselect("Pick  some fruits:",list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
    
-
-steamlit.header('Fruityvice Fruit Advice!')
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-steamlit.text(fruityvicce_response.json())
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
